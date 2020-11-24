ed07b1fcb5d762950c5b885a2bf4331ed0a8d81e
Louis Leffler <louisleffler@gmail.com>
2020-11-24 22:10:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,6 @@
-counter = 0
-array = [1,2,3,4,5]
- 
-while array[counter] do
-  puts array[counter]
-  counter += 1
+def square_array(array)
+  Enumerator.new do |y|
+    array.each { |e| y << e ** 2 }    
+  end
+  .take(array.length)
 end
