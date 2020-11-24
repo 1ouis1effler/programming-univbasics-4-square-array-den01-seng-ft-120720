1dd2b025a058e673b7e04199451a64716734aa76
Louis Leffler <louisleffler@gmail.com>
2020-11-24 22:14:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,3 @@
-def square_array(array)
-  Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }    
-  end
-  .take(array.length)
-end
-
-
 def square_array(numbers)
     numbers.map { |e| e ** 2 }
 end 
